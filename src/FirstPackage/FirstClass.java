3d5ec47365f8fe83f5b833c5e5143c4f32d9a7b4
Thatalent <jonathan.hudson2012@gmail.com>
2016-10-05 21:18:03 -0400

    Adding Homework problems

--- src/FirstPackage/FirstClass.java
+++ src/FirstPackage/FirstClass.java
@@ -8,26 +8,27 @@ public class FirstClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int integer = 76*6;
+		int integer = 9;
 		long longInteger = Long.MAX_VALUE;
 		short shortInteger = 32+76;
-		boolean bool = true;
-		bool = false;
+		boolean bool = 
+		bool == false;
 		char character = Character.MAX_VALUE;
 		//char characterA = 'Z';
 		float floatNumbers= 3/4;
 		double doubles = 75.9-0.9;
+		float example = (float)doubles;
 		byte bytes = Byte.MAX_VALUE;
-		integer = (int)doubles;
+		integer = doubles;
 		
 		//int a_Number = characterA;
 		//integer = FirstClass.integer + FirstClass.integer;
 		
 		
-		//System.out.println(characterA);
+		System.out.println(characterA);
 		System.out.println(longInteger);
 		System.out.println(integer);
-		//System.out.println(a_Number);
+		System.out.println(a_Number);
 		System.out.println(doubles);
 		System.out.println(floatNumbers);
 		System.out.println((int)character);
