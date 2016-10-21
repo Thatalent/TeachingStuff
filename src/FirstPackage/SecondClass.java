6fd85e003c476fc5863c96adaef9409654e7cf6f
Thatalent <jonathan.hudson2012@gmail.com>
2016-10-20 21:02:55 -0400

    Added new problems to SecondClass.java

---
+++ src/FirstPackage/SecondClass.java
@@ -0,0 +1,44 @@
+package FirstPackage;
+
+public class SecondClass {
+
+	public static void main(String[] args) {
+
+		int integer = 10 +2;
+		
+		int int2 = --integer;
+		
+		integer = 12;
+		
+		int int3 = integer--;
+		
+		// print r where r= n+3 and n = 2
+		
+		int n = 2/3;
+		
+		boolean r = n == 3;
+		
+		//use System.out.println() to print your results
+		
+		System.out.println(integer);
+		System.out.println(int2);
+		System.out.println(int3);
+		
+		// print String s with the value "Hello World"
+		
+		// print String sn with the value "11"
+		
+		// print n where n = int3/3
+		
+		// print the result of n <= int2
+		
+		// print the result of n != 2
+		
+		// print int h where h = n % 2
+		
+		// print the result of h % 1
+		
+		// print the result of h == h && h != n
+	}
+
+}
