86a59e75b5af4a4c6a0effde0d9aa4a08ed0b0b3
Bruce Watkins II <brucewatkins95@gmail.com>
2016-10-24 21:56:27 -0400

    Assignment Changes
    I used the methods on the second file to return the solutions after line 36.

--- src/FirstPackage/SecondClass.java
+++ src/FirstPackage/SecondClass.java
@@ -11,7 +11,7 @@ public class SecondClass {
 		integer = 12;
 		
 		int int3 = integer--;
-		
+		 
 		// print r where r= n+3 and n = 2
 		
 		int n = 2;
@@ -27,20 +27,31 @@ public class SecondClass {
 		System.out.println(int3);
 		
 		// print String s with the value "Hello World"
-		
+		String s = "Hello World";
+		System.out.println(s);
 		// print String sn with the value "11"
-		
+		String sn = "11";
+		System.out.println(sn);
 		// print n where n = int3/3
-		
+		float n1 = LikeSecondClass.divideThem(3, 3);
+		System.out.println(n1);
 		// print the result of n <= int2
-		
+		if (n <= 2) {
+			System.out.println(true);
+		}
 		// print the result of n != 2
-		
+		boolean n2 = LikeSecondClass.areTheyNotTheSame(1, 2);
+		System.out.println(n2);
 		// print int h where h = n % 2
-		
+		int h = n % 2;
+		System.out.println(h);
 		// print the result of h % 1
-		
+		System.out.println(h & 1);
 		// print the result of h == h && h != n
+		boolean h2 = LikeSecondClass.areTheyTheSame(h, h);
+		System.out.println(h2);
+		boolean h3 = LikeSecondClass.areTheyNotTheSame(h, n);
+		System.out.println(h3);
 	}
 
 }
